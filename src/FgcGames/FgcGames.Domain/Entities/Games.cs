8e62fccffdacd4d13ac82edb4760037a3846e68d
jonatas.paz <jonatasdamiao@hotmail.com>
2026-03-26 23:16:25 -0300

    Criando a estrutura do projeto e  configurando os pacotes abaixo
    - serilog
    - entity Framework
    - minimal api
    - swagger
    - Xunit

---
+++ src/FgcGames/FgcGames.Domain/Entities/Games.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FgcGames.Domain.Entities
+{
+    public class Games
+    {
+        public Games(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
--- src/FgcGames/FgcGames/Program.cs
+++ src/FgcGames/FgcGames/Program.cs
@@ -1,23 +1,36 @@
+using FgcGames.Api.Endpoints;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Error()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    c.RoutePrefix = "";
+});
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGamesEndpoints();
 
 app.Run();
