e9d1a9d4cfce39b2b27c02b11a80f467cf5176aa
jonatas.paz <jonatasdamiao@hotmail.com>
2026-03-26 23:16:25 -0300

    Criando a estrutura do projeto e  configurando os pacotes abaixo
    - serilog
    - entity Framework
    - minimal api
    - swagger
    - Xunit

---
+++ src/FgcGames/FgcGames.Infra/Database/FgcGamesContext.cs
@@ -0,0 +1,10 @@
+﻿using FgcGames.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class FgcGamesContext : DbContext
+{
+    public FgcGamesContext(DbContextOptions<FgcGamesContext> options)
+        : base(options) { }
+
+    public DbSet<Games> Games { get; set; }
+}
---
+++ src/FgcGames/FgcGames/Endpoints/GamesEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using FgcGames.Domain.Entities;
+
+namespace FgcGames.Api.Endpoints
+{
+    public static class GamesEndpoints
+    {
+        public static void MapGamesEndpoints(this WebApplication app)
+        {
+            app.MapGet("/games", async (FgcGamesContext db) => db.Games.ToList());
+
+            app.MapPost("/games", async (Games book, FgcGamesContext db) =>
+            {
+                db.Games.Add(book);
+                await db.SaveChangesAsync();
+
+                return Results.Created($"/games/{book.Id}", book);
+            });
+        }
+    }
+}
