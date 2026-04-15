40ae53aad44784c4ec7108486c13b973ee0004b5
Camila Melo Machado <camila_melomachado@hotmail.com>
2026-04-14 23:12:16 -0300

    ✨ feat: cria entidades base jogo e usuario usando DDD

---
+++ src/FgcGames.Domain/Entities/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using FgcGames.Domain.Enum;
+using FgcGames.Domain.Interfaces;
+using FgcGames.Domain.ValueObjects;
+
+namespace FgcGames.Domain.Entities;
+
+public class Usuario : Entity, IAggregateRoot
+{
+    public DateTime DataCriacao { get; private set; }
+    public string Nome { get; private set; }
+    public DateTime DataNascimento { get; private set; }
+    public Email Email { get; private set; }
+    public Senha Senha { get; private set; }
+    public UserRole Role { get; private set; }
+    public bool Inativo { get; private set; }
+
+    public Usuario(string nome, DateTime dataNascimento, Email email, Senha senha, UserRole userRole)
+    {
+        DataCriacao = DateTime.UtcNow;
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        Email = email;
+        Senha = senha;
+        Role = userRole;
+    }
+
+    public void AlterarSenha(Senha novaSenha)
+    {
+        //implementar
+        Senha = novaSenha;
+    }
+
+    public void Inativar() => Inativo = true;
+
+    public bool EhAdmin() => Role == UserRole.Admin;
+
+    public int CalcularIdade()
+    {
+        //implementar
+        return 1;
+    }
+}
---
+++ src/FgcGames.Domain/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,7 @@
+﻿namespace FgcGames.Domain.Interfaces;
+
+/// <summary>
+/// Interface de marcação para definir que uma Entidade é uma Raiz de Agregado.
+/// Repositórios devem ser criados apenas para Raízes de Agregado.
+/// </summary>
+public interface IAggregateRoot { }
