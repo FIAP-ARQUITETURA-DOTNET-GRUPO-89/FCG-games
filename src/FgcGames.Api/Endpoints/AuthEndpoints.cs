da6456e8de42da5f08809f8cd1c27635586a6a8d
Camila Melo Machado <camila_melomachado@hotmail.com>
2026-04-14 23:33:46 -0300

    ✨ feat: cria classes base de repo e minimal api

---
+++ src/FgcGames.Api/Endpoints/AuthEndpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace FgcGames.Api.Endpoints;
+
+public static class AuthEndpoints
+{
+    public static void MapAuthEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/auth").WithTags("Auth");
+    }
+}
---
+++ src/FgcGames.Api/Endpoints/JogoEndpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace FgcGames.Api.Endpoints;
+
+public static class JogoEndpoints
+{
+    public static void MapJogoEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/jogos").WithTags("Jogos");
+    }
+}
