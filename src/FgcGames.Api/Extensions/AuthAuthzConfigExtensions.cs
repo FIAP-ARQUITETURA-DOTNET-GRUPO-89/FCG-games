04c348f726b90aa2d3de4088052b3ab333ae0682
Camila Melo Machado <camila_melomachado@hotmail.com>
2026-04-20 22:43:46 -0300

    ✨ feat(auth/authz): incluir passos iniciais de autenticação e autorização

---
+++ src/FgcGames.Api/Extensions/AuthAuthzConfigExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FgcGames.Api.Extensions;
+
+public static class AuthAuthzConfigExtensions
+{
+    public static IServiceCollection AuthAuthzConfig(this IServiceCollection services)
+    {
+        const string issuer = "FgcGames-Issuer";
+        const string securityKey = "FgcGames_Secret_Key_2026_High_Security_Token";
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = issuer,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey))
+            };
+        });
+
+        services.AddAuthorizationBuilder()
+                .AddPolicy("Admin", policy => policy.RequireRole("Admin"))
+                .AddPolicy("Usuario", policy => policy.RequireRole("Usuario"));
+
+        return services;
+    }
+}
---
+++ src/FgcGames.Infra/Services/TokenService.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using FgcGames.Application.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FgcGames.Infra.Services;
+
+public class TokenService : ITokenService
+{
+    public string GenerateJwtToken(string email, string role)
+    {
+        const string issuer = "FgcGames-Issuer";
+        const string securityKey = "FgcGames_Secret_Key_2026_High_Security_Token";
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, email),
+            new Claim(ClaimTypes.Role, role),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: issuer,
+            audience: null,
+            claims: claims,
+            expires: DateTime.Now.AddHours(2),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
