8da50a02831fbc7ca0e5816294171578eaf0b1e8
Camila Melo Machado <camila_melomachado@hotmail.com>
2026-04-23 21:13:30 -0300

    🔧 chore: ajustes para trabalhar com variaveis de ambientes e appsettings separados

--- src/FgcGames.AppHost/AppHost.cs
+++ src/FgcGames.AppHost/AppHost.cs
@@ -2,7 +2,9 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.Environment.IsEnvironment("Testing")
+var isTesting = builder.Environment.IsEnvironment("Testing");
+
+var postgres = isTesting
     ? builder.AddPostgres("Postgres")
         .WithLifetime(ContainerLifetime.Session)
         .AddDatabase("Default", "fgcgames-db")
@@ -12,6 +14,7 @@ var postgres = builder.Environment.IsEnvironment("Testing")
         .AddDatabase("Default", "fgcgames-db");
 
 builder.AddProject<Projects.FgcGames_Api>("fgcgames-api")
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName)
     .WithReference(postgres)
     .WaitFor(postgres);
 
